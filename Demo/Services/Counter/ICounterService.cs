f96f9a092f0e03d80e5d27246cc779a08d295008
Selçuk Arslan <me@sarslan.net>
2019-04-14 20:16:50 -0400

    IService is added.

---
+++ Demo/Services/Counter/ICounterService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arslan.Net.Service.Demo.Services
+{
+    public interface ICounterService
+    {
+        int Value();
+        int Decrease();
+        int Increase();
+        int Reset();
+    }
+}
